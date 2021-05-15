4cf2e5f1eafab75bd296be38219280b973b24b25
Zach11111 <zachrochette@hotmail.com>
2021-05-15 12:44:17 -0600

    commit

--- lesson4/js/myscripts.js
+++ lesson4/js/myscripts.js
@@ -1,1 +1,5 @@
 document.querySelector("#last_update").textContent =document.lastModified;
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+}
