795d969932fc6a2e3fb9f8ca54299c15f6baea88
Zach11111 <zachrochette@hotmail.com>
2021-06-04 18:28:26 -0600

    commit

--- lesson7/js/lesson7.js
+++ lesson7/js/lesson7.js
@@ -47,17 +47,17 @@ if (daylist[day] == 'Friday') {
   }
 
 //The following code is for the days since last visit to the site
-var currentTime = today.getTime();
+var currentTime = d.getTime();
 
 //Checks last visited
 if (!localStorage.getItem("lastVisited")) {
   localStorage.setItem("lastVisited", currentTime);
-  document.querySelector("#days-since-last-visit").innerHTML = "Days Since Last Visit: 0"
+  document.querySelector("#days-since-visit").innerHTML = "Days Since Last Visit: 0"
 }
 else {
   var lastVisit = localStorage.getItem("lastVisited");
   localStorage.setItem("lastVisited", currentTime);
   //Dividing by 86,400,000 because time stamp is given in miliseconds and there are 1000 miliseconds in a second, 60 seconds in a minute, 60 minutes in an hour, and 24 hours in a day (1000 * 60 * 60 * 24)
   var daysSinceVisit = Math.floor((currentTime - lastVisit) / 86400000);
-  document.querySelector("#days-since-last-visit").innerHTML = "Days Since Last Visit: " + daysSinceVisit;
+  document.querySelector("#days-since-visit").innerHTML = "Days Since Last Visit: " + daysSinceVisit;
 }
