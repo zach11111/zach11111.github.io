30b094bd47ff6e2e067d6439df588a7512c3e100
Zach11111 <zachrochette@hotmail.com>
2021-06-26 14:53:10 -0600

    commit

---
+++ lesson10/js/prestonapi.js
@@ -0,0 +1,17 @@
+const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=8e352917079519d2ec55d70cab208751&units=imperial";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('currently').textContent = jsObject.main.temp;
+    document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('wind_speed').textContent = jsObject.wind.speed;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+});
