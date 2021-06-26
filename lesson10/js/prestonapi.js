ab1d3fa41bb5b928026b97ea0ebb211a7c6d245f
Zach11111 <zachrochette@hotmail.com>
2021-06-26 15:44:56 -0600

    commit

--- lesson10/js/prestonapi.js
+++ lesson10/js/prestonapi.js
@@ -4,16 +4,21 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-
     document.getElementById('current_temp').textContent = jsObject.main.temp;
     document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('wind_speed').textContent = jsObject.wind.speed;
+
+// const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+// const desc = jsObject.weather[0].description;  // note how we reference the weather array
+// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+// document.getElementById('icon').setAttribute('alt', desc);
+
 });
 
-    /* Five Day Forecast */
-
-    fetch(apiForecastURL)
+/* 5 Days Forecast*/
+fetch(apiForecastURL)
     .then(response => response.json())
     .then((jsObject) => {
                
@@ -66,5 +71,3 @@ fetch(apiURL)
 	        day++;	  
 	});
 });
-
-
