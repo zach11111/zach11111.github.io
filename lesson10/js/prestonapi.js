84d3e59872007c975b1e49e78e0b1d93836a8810
Zach11111 <zachrochette@hotmail.com>
2021-06-26 15:18:46 -0600

    commit

--- lesson10/js/prestonapi.js
+++ lesson10/js/prestonapi.js
@@ -14,29 +14,27 @@ const desc = jsObject.weather[0].description;  // note how we reference the weat
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
-});
+
     /* Five Day Forecast */
 
-    const fivedayforecast = jsObject.list.filter((forecast) =>
-      forecast.dt_txt.includes("18:00:00")
-    );
+const fivedayforecast = jsObject.list.filter((forecast) =>
+  forecast.dt_txt.includes("18:00:00")
+);
+for (let i = 0; i < fivedayforecast.length; i++) {
+  var forecast = fivedayforecast[i];
+  var tempElement = "temp" + i;
+  var iconElement = "icon" + i;
+  var dayElement = "day" + i;
+  var forecastDate = new Date(forecast.dt_txt);
+  var forecastDay = days[forecastDate.getDay()];
+  const desc = forecast.weather[0].description;
+  const iconsrc = 
+  "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+  document.getElementById(dayElement).textContent = forecastDay;
+  document.getElementById(iconElement).setAttribute("src", iconsrc);
+  document.getElementById(iconElement).setAttribute("alt", desc);
+  document.getElementById(tempElement).textContent = forecast.main.temp;
+      
 
-    for (let i = 0; i < fivedayforecast.length; i++) {
-      var forecast = fivedayforecast[i];
-      var tempElement = "temp" + i;
-      var iconElement = "icon" + i;
-      var dayElement = "day" + i;
-      var forecastDate = new Date(forecast.dt_txt);
-      var forecastDay = days[forecastDate.getDay()];
+  }});
 
-      const desc = forecast.weather[0].description;
-      const iconsrc = 
-      "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
-
-      document.getElementById(dayElement).textContent = forecastDay;
-      document.getElementById(iconElement).setAttribute("src", iconsrc);
-      document.getElementById(iconElement).setAttribute("alt", desc);
-      document.getElementById(tempElement).textContent = forecast.main.temp;
-
-          
-    };
