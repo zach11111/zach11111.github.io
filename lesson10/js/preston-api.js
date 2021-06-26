5bed64d671c9a0778b6b2411bcd7d3a68ab26a61
Zach11111 <zachrochette@hotmail.com>
2021-06-26 14:18:15 -0600

    commit

--- lesson10/js/preston-api.js
+++ lesson10/js/preston-api.js
@@ -1,5 +1,4 @@
 const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=8e352917079519d2ec55d70cab208751=imperial";
-
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
@@ -9,13 +8,17 @@ fetch(apiURL)
         fhigh = Math.round((jsObject.main.temp_max - 273.15) * (9/5) + 32)
         document.getElementById('high').textContent = fhigh;
         document.getElementById('humidity').textContent = jsObject.main.humidity;
-        document.getElementById('wind_speed').textContent = jsObject.wind.speed;
+        document.getElementById('WindSpeed').textContent = jsObject.wind.speed;
+
+        let currentChill = windChill(ftemp, jsObject.wind.speed);
+
+        document.querySelector('#windChill').textContent = currentChill;
+
     });
 
 const days = ["1", "2", "3", "4", "5"];
 const dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=8e352917079519d2ec55d70cab208751=imperial";
-
 fetch(forecast)
     .then((response) => response.json())
     .then((jsObject) => {
@@ -33,3 +36,4 @@ fetch(forecast)
             i++
         })
     })
+        
