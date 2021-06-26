df7c6b5202aaeb3d9d914802bd006a0d2dcaf74d
Zach11111 <zachrochette@hotmail.com>
2021-06-26 14:57:02 -0600

    commit

---
+++ lesson10/js/windchill.js
@@ -0,0 +1,12 @@
+let t = document.getElementById("currently").innerHTML;
+let s = document.getElementById("wind_speed").innerHTML;
+var result;
+
+if(t <= 45 && s >= 12){
+    result = Math.round(35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** 0.16)));
+    
+document.getElementById("windchill").innerHTML = result;
+}
+else{
+    document.getElementById("windchill").innerHTML = "N/A";
+}
