1249376f1cbd7b4f548b6a20b68d068d3ecada6f
Zach11111 <zachrochette@hotmail.com>
2021-07-02 03:52:48 -0600

    commit

--- lesson11/js/fish-haven-api.js
+++ lesson11/js/fish-haven-api.js
@@ -1,26 +1,31 @@
-
-const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=8e352917079519d2ec55d70cab208751&units=imperial";
+const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=8e352917079519d2ec55d70cab208751&units=imperial";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('curr').innerText = jsObject.weather[0].description;
-    document.getElementById('tempH').innerText = jsObject.main.temp_max;
-    document.getElementById('hum').innerText = jsObject.main.humidity;
-    document.getElementById('wSpeed').innerText = jsObject.wind.speed;
-  });
+    document.getElementById('current_temp').textContent = jsObject.main.temp;
+    document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('wind_speed').textContent = jsObject.wind.speed;
 
+// const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+// const desc = jsObject.weather[0].description;  // note how we reference the weather array
+// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+// document.getElementById('icon').setAttribute('alt', desc);
 
-const apiURLforecast = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=8e352917079519d2ec55d70cab208751&units=imperial";
+});
+
+const apiURLforecast = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=8e352917079519d2ec55d70cab208751&units=imperial";
     fetch(apiURLforecast)
     .then((response) => response.json())
     .then((jsObject) => {
         const forecast = jsObject['list'];
 
         for (let i = 3; i < forecast.length; i += 8) {
-        document.getElementById(`imgForecast${i}`).setAttribute('src', "https://openweathermap.org/img/w/" + forecast[i].weather[0].icon + ".png");
-        document.getElementById(`tempForecast${i}`).innerText = forecast[i].main.temp;
+        document.getElementById(`icon${i}`).setAttribute('src', "https://openweathermap.org/img/w/" + forecast[i].weather[0].icon + ".png");
+        document.getElementById(`temp${i}`).innerText = forecast[i].main.temp;
         }
 
 });
