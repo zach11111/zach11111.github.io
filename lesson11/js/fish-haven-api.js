b161a4f117188992f3ddb7a5a1a2750ce6c7fa18
Zach11111 <zachrochette@hotmail.com>
2021-07-02 03:45:56 -0600

    commit

--- lesson11/js/fish-haven-api.js
+++ lesson11/js/fish-haven-api.js
@@ -4,22 +4,15 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current_temp').textContent = jsObject.main.temp;
-    document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
-    document.getElementById('humidity').textContent = jsObject.main.humidity;
-    document.getElementById('wind_speed').textContent = jsObject.wind.speed;
+    document.getElementById('curr').innerText = jsObject.weather[0].description;
+    document.getElementById('tempH').innerText = jsObject.main.temp_max;
+    document.getElementById('hum').innerText = jsObject.main.humidity;
+    document.getElementById('wSpeed').innerText = jsObject.wind.speed;
+  });
 
-// const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-// const desc = jsObject.weather[0].description;  // note how we reference the weather array
-// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-// document.getElementById('icon').setAttribute('alt', desc);
 
-});
-
-/* 5 Days Forecast*/
-
-const apiURLforecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=8e352917079519d2ec55d70cab208751';
+const apiURLforecast = 
+'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=4dc63840c593a4c37e15eb58b499f77f';
     fetch(apiURLforecast)
     .then((response) => response.json())
     .then((jsObject) => {
