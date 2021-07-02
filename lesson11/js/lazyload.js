5843674bc513a9c6fa040cbbc87a1d8a7547f22d
Zach11111 <zachrochette@hotmail.com>
2021-07-02 02:29:36 -0600

    commit

---
+++ lesson11/js/lazyload.js
@@ -0,0 +1,29 @@
+
+const allImages = document.querySelectorAll("img[data-src]");
+
+const loadImages = (img) => {
+  img.setAttribute("src", img.getAttribute("data-src"));
+  img.onload = () => {
+    img.removeAttribute("data-src");
+  }
+}
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  
+  allImages.forEach((img) => {
+    observer.observe(img);
+  });
+}
+else {
+  allImages.forEach((img) => {
+    loadImages(img);
+  });
+}
