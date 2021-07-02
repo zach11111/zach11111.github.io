f903d808ca4ab42335ad927b5aafd9e3a7d5746d
Zach11111 <zachrochette@hotmail.com>
2021-07-02 03:22:28 -0600

    commit

--- lesson11/js/prestonapi.js
+++ lesson11/js/prestonapi.js
@@ -38,3 +38,32 @@ fetch(forecast)
             i++
         })
     })
+
+         /* Preston Town Events */
+
+         const townListURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+         fetch(townListURL)
+         .then(function (response) {
+             return response.json();
+         })
+         
+         .then(function (townlist) {
+          //   console.table(townlist);
+             const towns = townlist["towns"];
+             for (i=0; i<towns.length; i++) {
+                 if (towns[i].name == "Preston") {
+                     let eventCard = document.createElement("section");
+                     let townHeading = document.createElement("h4");  
+                     let townEvents = document.createElement("p");        
+         
+                 townHeading.textContent = "Upcoming Events";
+                 townEvents.textContent = towns[i].events;
+                 
+                 eventCard.appendChild(townHeading);
+                 eventCard.appendChild(townEvents);        
+         
+                 document.querySelector("div.eventCard").appendChild(eventCard); 
+                 
+             }}
+         });
