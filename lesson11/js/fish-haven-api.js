7092fe65747e0345aeffc8664e0424a84dca87fa
Zach11111 <zachrochette@hotmail.com>
2021-07-02 04:01:19 -0600

    commit

--- lesson11/js/fish-haven-api.js
+++ lesson11/js/fish-haven-api.js
@@ -1,13 +1,14 @@
 const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=8e352917079519d2ec55d70cab208751&units=imperial";
+const townName = "Fish Haven";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current_temp').innerText = jsObject.weather[0].description;
-    document.getElementById('high').innerText = jsObject.main.temp_max;
-    document.getElementById('humidity').innerText = jsObject.main.humidity;
-    document.getElementById('wind_speed').innerText = jsObject.wind.speed;
+    document.getElementById('current_temp').textContent = jsObject.main.temp;
+    document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('wind_speed').textContent = jsObject.wind.speed;
 
 // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 // const desc = jsObject.weather[0].description;  // note how we reference the weather array
@@ -17,19 +18,27 @@ fetch(apiURL)
 
 });
 
-
-const apiURLforecast = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=8e352917079519d2ec55d70cab208751&units=imperial";
-    fetch(apiURLforecast)
+/* 5 Days Forecast*/
+const days = ["1", "2", "3", "4", "5"];
+const dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+const forecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=8e352917079519d2ec55d70cab208751';
+fetch(forecast)
     .then((response) => response.json())
     .then((jsObject) => {
-        const forecast = jsObject['list'];
-
-        for (let i = 3; i < forecast.length; i += 8) {
-        document.getElementById(`imgForecast${i}`).setAttribute('src', "https://openweathermap.org/img/w/" + forecast[i].weather[0].icon + ".png");
-        document.getElementById(`tempForecast${i}`).innerText = forecast[i].main.temp;
-        }
-
-});
+        console.log(jsObject);
+        const nextfive = jsObject.list.filter(dt => dt.dt_txt.includes('18:00:00'));
+        let img = 'https://openweathermap.org/img/w/';
+        i = 0
+        nextfive.forEach(day => {
+            
+            let forcast = new Date(day.dt_txt);
+            dayOfWeek = days[i];
+            document.getElementById(dayOfWeek).textContent = dayNames[forcast.getDay()];
+            ftemp = Math.round((day.main.temp - 273.15) * (9/5) + 32);
+            document.getElementById("temp" + days[i]).textContent = ftemp;
+            i++
+        })
+    })
 
      /* Fish Haven Town Events */
 
@@ -41,7 +50,7 @@ const apiURLforecast = "https://api.openweathermap.org/data/2.5/weather?id=55850
   })
   
   .then(function (townlist) {
-   //   console.table(townlist);
+
       const towns = townlist["towns"];
       for (i=0; i<towns.length; i++) {
           if (towns[i].name == "Fish Haven") {
