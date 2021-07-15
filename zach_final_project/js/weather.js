a35236cc54d13231986989bf6fae23bf3b19ca8c
Zach11111 <zachrochette@hotmail.com>
2021-07-15 03:56:55 -0600

    commit

--- zach_final_project/js/weather.js
+++ zach_final_project/js/weather.js
@@ -1,10 +1,11 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=20.68&lon=-53.89&exclude=hourly,minute&appid=8e352917079519d2ec55d70cab208751&units=imperial"
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=20.68&lon=-53.89&exclude=hourly,minute&appid=8e352917079519d2ec55d70cab208751&units=imperial&lang=en"
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
         document.getElementById('current-temp').textContent = jsObject.current.temp;
         document.getElementById('humidity').textContent = jsObject.current.humidity;
+        document.getElementById('condition').textContent = jsObject.current.weather.description;
 
         // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
         // const desc = jsObject.weather[0].description;  // note how we reference the weather array
