1c99b92761b3dcca9fe6df9e497525e154ae70fe
Zach11111 <zachrochette@hotmail.com>
2021-07-15 03:46:21 -0600

    commit

--- zach_final_project/js/weather.js
+++ zach_final_project/js/weather.js
@@ -3,9 +3,9 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('current-temp').textContent = jsObject.current.temp;
 
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.current.weather.icon + '.png';  // note the concatenation
         const desc = jsObject.weather[0].description;  // note how we reference the weather array
         document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
         document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
