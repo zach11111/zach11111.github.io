28717c6ac3d88b03bd8e8b56796479b71107a5e8
Zach11111 <zachrochette@hotmail.com>
2021-07-15 03:31:37 -0600

    commit

--- zach_final_project/js/weather.js
+++ zach_final_project/js/weather.js
@@ -1,15 +1,6 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=43.6724&lon=-111.9150&exclude=hourly,minute&appid=8e352917079519d2ec55d70cab208751&units=imperial"
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=20.68&lon=-53.89&exclude=hourly,minute&appid=8e352917079519d2ec55d70cab208751&units=imperial"
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-    //     document.getElementById('current-temp').textContent = jsObject.daily.temp.max;
-    //     document.getElementById('image-src').textContent = imagesrc;
-    //     document.getElementById('icon').setAttribute('src', imagesrc); 
-    //     document.getElementById('icon').setAttribute('alt', desc);
-    //     document.getElementById('alert').textContent = jsObject.alerts.event;
-
-    // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-    // const desc = jsObject.weather[0].description;
-
 });
