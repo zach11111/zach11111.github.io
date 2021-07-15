cd5cf5d9d0592f0adadc612ee5caa4438728e223
Zach11111 <zachrochette@hotmail.com>
2021-07-15 03:19:26 -0600

    commit

--- zach_final_project/js/weather.js
+++ zach_final_project/js/weather.js
@@ -7,7 +7,7 @@ fetch(apiURL)
         document.getElementById('image-src').textContent = imagesrc;
         document.getElementById('icon').setAttribute('src', imagesrc); 
         document.getElementById('icon').setAttribute('alt', desc);
-        document.getElementById('alert').textContent = jsObject.alerts;
+        document.getElementById('alert').textContent = jsObject.alerts.event;
 
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
     const desc = jsObject.weather[0].description;
