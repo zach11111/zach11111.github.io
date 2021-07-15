b10faf8a6bb00a1829f415efa433569c2243bcb1
Zach11111 <zachrochette@hotmail.com>
2021-07-15 04:12:26 -0600

    commit

--- zach_final_project/js/weather.js
+++ zach_final_project/js/weather.js
@@ -5,10 +5,8 @@ fetch(apiURL)
         console.log(jsObject);
         document.getElementById('current-temp').textContent = jsObject.current.temp;
         document.getElementById('humidity').textContent = jsObject.current.humidity;
+        document.getElementById('condition').textContent = jsObject.current.weather.main;
         document.getElementById('wind_speed').textContent = jsObject.current.wind_speed;
-
-        const desc = jsObject.current.weather.description;
-        document.getElementById('condition').setAttribute('alt', desc);
         
 
         // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
