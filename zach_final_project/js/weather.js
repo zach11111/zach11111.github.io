e0b73ad7f300460308ea5cc2dec694e232a63bb0
Zach11111 <zachrochette@hotmail.com>
2021-07-17 19:44:31 -0600

    commit

--- zach_final_project/js/weather.js
+++ zach_final_project/js/weather.js
@@ -17,21 +17,22 @@ fetch(apiURL)
         
 });
 
+    /* 3 day forecast */
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=20.68&lon=-53.89&appid=8e352917079519d2ec55d70cab208751&units=imperial";
 fetch(forecastURL)
-  .then((response) => response.json())
-  .then ((rigbyForecast) => {
-    const dayofweek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const threedays = rigbyForecast.list.filter(item => item.dt_txt.includes("18:00:00"));
-    let num = 1;
-    let imgurl = "https://openweathermap.org/img/wn/";
-    threedays.forEach(day => {
-      let forecastday = new Date(day.dt_txt);
-      document.getElementById("day" + num).textContent = dayofweek[forecastday.getDay()];
-      document.getElementById("temp" + num).textContent = Math.round(day.main.temp);
-      let icon = document.getElementById("icon" + num);
-      icon.setAttribute("src", imgurl + day.weather[0].icon + "@2x.png");
-      icon.setAttribute("alt", day.weather[0].description);
-      num ++;
+    .then((response) => response.json())
+    .then ((rigbyForecast) => {
+        const dayofweek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const threedays = rigbyForecast.list.filter(item => item.dt_txt.includes("18:00:00"));
+        let num = 1;
+        let imgurl = "https://openweathermap.org/img/wn/";
+        threedays.forEach(day => {
+            let forecastday = new Date(day.dt_txt);
+            document.getElementById("day" + num).textContent = dayofweek[forecastday.getDay()];
+            document.getElementById("temp" + num).textContent = Math.round(day.main.temp);
+            let icon = document.getElementById("icon" + num);
+            icon.setAttribute("src", imgurl + day.weather[0].icon + "@2x.png");
+            icon.setAttribute("alt", day.weather[0].description);
+            num ++;
     });
   });
