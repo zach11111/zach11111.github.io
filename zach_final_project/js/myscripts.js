4b2f6a96a439b12884dbfa9d2c9cda7169b18720
Zach11111 <zachrochette@hotmail.com>
2021-07-18 03:09:20 -0600

    commit

--- zach_final_project/js/myscripts.js
+++ zach_final_project/js/myscripts.js
@@ -40,3 +40,20 @@ function toggleMenu()
 {
     document.getElementsByClassName("nav")[0].classList.toggle("responsive")
 }
+
+function ajaxpost(){
+    // (A) GET FORM DATA
+    var data = new FormData();
+    data.append("name", document.getElementById("user-name").value);
+    data.append("email", document.getElementById("user-email").value);
+   
+    // (B) AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "0-dummy.php");
+    // What to do when server responds
+    xhr.onload = function(){ console.log(this.response); };
+    xhr.send(data);
+   
+    // (C) PREVENT HTML FORM SUBMIT
+    return false;
+  }
