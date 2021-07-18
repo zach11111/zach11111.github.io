f98f1c5d96fc47b9414ee441122eaab14961043b
Zach11111 <zachrochette@hotmail.com>
2021-07-17 19:26:32 -0600

    commit

--- zach_final_project/js/weather.js
+++ zach_final_project/js/weather.js
@@ -18,24 +18,21 @@ fetch(apiURL)
 
 
         /*    Three Day Forecast   */
-
+        fetch(apiURL)
+        .then((response) => response.json())
+        .then ((forecast) => {
+          const dayofweek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+          const days = forecast.list.filter(item => item.dt_txt.includes("18:00:00"));
+          let num = 1;
+          let imagesrc = "https://openweathermap.org/img/wn/" + jsObject.current.weather[0].icon + '@2x.png';
+          days.forEach(day => {
+            let forecastday = new Date(day.dt_txt);
+            document.getElementById("day" + num).textContent = dayofweek[forecastday.getDay()];
+            document.getElementById("temp" + num).textContent = Math.round(day.main.temp);
+            let icon = document.getElementById("icon" + num);
+            icon.setAttribute("src", imagesrc + day.weather[0].icon + "@2x.png");
+            icon.setAttribute("alt", day.weather[0].description);
+            num ++;
+          });
+        });
 });
-
-const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=8e352917079519d2ec55d70cab208751&units=imperial";
-fetch(forecastURL)
-  .then((response) => response.json())
-  .then ((forecast) => {
-    const dayofweek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const days = forecast.list.filter(item => item.dt_txt.includes("18:00:00"));
-    let num = 1;
-    let imagesrc = "https://openweathermap.org/img/wn/" + jsObject.current.weather[0].icon + '@2x.png';
-    days.forEach(day => {
-      let forecastday = new Date(day.dt_txt);
-      document.getElementById("day" + num).textContent = dayofweek[forecastday.getDay()];
-      document.getElementById("temp" + num).textContent = Math.round(day.main.temp);
-      let icon = document.getElementById("icon" + num);
-      icon.setAttribute("src", imagesrc + day.weather[0].icon + "@2x.png");
-      icon.setAttribute("alt", day.weather[0].description);
-      num ++;
-    });
-  });
