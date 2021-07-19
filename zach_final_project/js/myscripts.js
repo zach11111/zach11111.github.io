9988a9937a65368f34d99b694f516b2f96f38a1b
Zach11111 <zachrochette@hotmail.com>
2021-07-18 18:31:41 -0600

    commit

--- zach_final_project/js/myscripts.js
+++ zach_final_project/js/myscripts.js
@@ -62,13 +62,5 @@ function visitContact(){
 // })
 // .then(data => console.log(data));
 
-fetch("./json/directory.json")
-.then((response) => response.json())
-    .then((company) => {
-        console.log(jsObject);
-        document.getElementById('business').textContent = company.company.name;
-        document.getElementById('phone').textContent = company.company.phone;
-        document.getElementById('email').textContent = company.company.email;
-        document.getElementById('website').textContent = company.company.website;
-        // document.getElementById('img').textContent = jsObject.alerts;
-    });
+var mydata = JSON.parse(directory);
+alert(mydata[0].name);
