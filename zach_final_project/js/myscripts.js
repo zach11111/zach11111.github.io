35f5a22177e1caa3ac0964246eed3972eadb0ba7
Zach11111 <zachrochette@hotmail.com>
2021-07-19 06:22:29 -0600

    commit

--- zach_final_project/js/myscripts.js
+++ zach_final_project/js/myscripts.js
@@ -77,7 +77,7 @@ function visitContact(){
 function removeWeather() {
     var element = document.getElementById("myDIV");
     element.classList.remove("weather-card");
-    onclick=this.style.display=none;
+    onclick=button.style.display=none;
   }
 
     // button.style.display = "none";
