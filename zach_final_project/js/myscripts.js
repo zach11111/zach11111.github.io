4c6dbeb3cb1634e06b2f7712e42d930e293caf49
Zach11111 <zachrochette@hotmail.com>
2021-07-21 03:51:50 -0600

    commit

--- zach_final_project/js/myscripts.js
+++ zach_final_project/js/myscripts.js
@@ -68,22 +68,37 @@ function upComingEvents(){
 //         document.getElementById('email').textContent = jsObject.company[0].email;
 //         document.getElementById('website').textContent = jsObject.company[0].website;
 // });
-const requestURL = "https://zach11111.github.io/zach_final_project/json/directory.json";
-const company = jsonObject['company']
-fetch(requestURL)
+  const requestURL = "https://zach11111.github.io/zach_final_project/json/directory.json";
+
+  fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
+  
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    for (let i = 0; i < company.length; i++ ) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        h2.textContent = company[i].name;
-        card.appendChild(h2);
-        document.querySelector('div.cards').appendChild(card);
-        image.setAttribute('src', prophets[i].img);
-  }});
+    console.table(jsonObject);
+    const company = jsonObject["company"];
+    for (let i = 0; i < prophets.length; i++) {
+        let card = document.createElement("section");
+        let name = document.createElement("h2");  
+        let date = document.createElement("p");
+        let place = document.createElement("p");   
+        let image = document.createElement("img");
+  
+      name.textContent = company[i].name;
+      date.textContent = "Phone:" + " " + company[i].phone;
+      place.textContent = "Email:" + " " + company[i].email;
+      image.setAttribute("src", company[i].img);
+      image.setAttribute("alt", company[i].name);
+  
+      card.appendChild(name);
+      card.appendChild(phone);
+      card.appendChild(email);
+      card.appendChild(image);
+  
+      document.querySelector("div.cards").appendChild(card);            
+    }
+  });
 //Print each business card
 
 // Remove weather-card from screen 
