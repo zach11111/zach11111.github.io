ac9fbba69aa4c9ad52497bb6bd017ff9f9652af7
Zach11111 <zachrochette@hotmail.com>
2021-07-21 03:40:42 -0600

    commit

--- zach_final_project/js/myscripts.js
+++ zach_final_project/js/myscripts.js
@@ -59,17 +59,33 @@ function upComingEvents(){
     window.location="https://www.eventbrite.com/d/id--rigby/events/";
 }
 //Business json
-fetch("https://zach11111.github.io/zach_final_project/json/directory.json")
-    .then((response) => response.json())
-    .then((jsObject) => {
-        console.log(jsObject);
-        document.getElementById('name').textContent = jsObject.company[0].name;
-        document.getElementById('phone').textContent = jsObject.company[0].phone;
-        document.getElementById('email').textContent = jsObject.company[0].email;
-        document.getElementById('website').textContent = jsObject.company[0].website;
-});
+// fetch("https://zach11111.github.io/zach_final_project/json/directory.json")
+//     .then((response) => response.json())
+//     .then((jsObject) => {
+//         console.log(jsObject);
+//         document.getElementById('name').textContent = jsObject.company[0].name;
+//         document.getElementById('phone').textContent = jsObject.company[0].phone;
+//         document.getElementById('email').textContent = jsObject.company[0].email;
+//         document.getElementById('website').textContent = jsObject.company[0].website;
+// });
+const requestURL = "https://zach11111.github.io/zach_final_project/json/directory.json";
+const company = jsonObject['company']
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    for (let i = 0; i < company.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
 
+        h2.textContent = company[i].name;
 
+        card.appendChild(h2);
+
+        document.querySelector('div.cards').appendChild(card);
+  }});
 //Print each business card
 
 // Remove weather-card from screen 
