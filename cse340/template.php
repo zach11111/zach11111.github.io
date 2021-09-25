66bb92eba1a277fea99ea20c8e9b66f2be281392
ZachRochette <zachrochette@hotmail.com>
2021-09-25 00:04:40 -0600

    commit

---
+++ cse340/template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <title>PHP Motors</title>
+    <meta charset="utf-8" />
+    <meta name="author" content="Zach Rochette" />
+    <meta name="description" content="Ehancement 1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="css/normalize.css" />
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <nav>
+
+    </nav>
+
+    <main>
+
+    </main>
+</body>
