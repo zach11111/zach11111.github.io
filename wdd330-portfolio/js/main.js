b18435b8b93ad13e1f69d61caccd732a2abdfc9a
ZachRochette <zachrochette@hotmail.com>
2022-01-07 16:24:24 -0700

    commit

---
+++ wdd330-portfolio/js/main.js
@@ -0,0 +1,19 @@
+const links = [
+    { 
+        label: "Week1 notes", 
+        url: "/WDD330/Week 1/index.html" 
+    }
+];
+let list = document.getElementById("links");
+
+
+links.forEach((item) => {
+
+    let li = document.createElement("li");
+    list.appendChild(li);
+    let label = document.createElement("a");
+    label.setAttribute("href", item.url);
+    label.innerHTML = item.label; 
+    li.appendChild(label);
+    
+});
