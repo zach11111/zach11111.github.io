8ad534d7b32bbdb35832827306d738221013fdc1
ZachRochette <zachrochette@hotmail.com>
2022-03-21 23:30:48 -0600

    commit

---
+++ wdd330-portfolio/Final_project/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "3e6029535c12352d468e014524447e8a";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
