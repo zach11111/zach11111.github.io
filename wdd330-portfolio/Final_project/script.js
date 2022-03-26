f3355eb2875f795977b3c1f1542259260638d38c
ZachRochette <zachrochette@hotmail.com>
2022-03-26 01:36:14 -0600

    commit

---
+++ wdd330-portfolio/Final_project/script.js
@@ -0,0 +1,74 @@
+// TMDB API
+//TMDB api_key=3e6029535c12352d468e014524447e8a
+const API_KEY = 'api_key=3e6029535c12352d468e014524447e8a';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+ API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+ API_KEY;
+
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        showMovies(data.results);
+
+    })
+
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL + poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+
+        main.appendChild(movieEl);
+    })
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function getColor(vote) {
+    if(vote>= 7){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    setGenre();
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
